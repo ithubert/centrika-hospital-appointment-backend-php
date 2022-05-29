88c735264bf3524e221a134b534a33a1d31168c8
ithubert <hubertithug@gmail.com>
2022-05-29 19:51:35 +0200

    code cleaning

--- configs/token.php
+++
@@ -1,7 +0,0 @@
-<?php
-require("../configs/header.php");
-
-if (isset($_POST["version"])) {
-    $version = $_POST["version"];
-
-}
--- temp/v1/sms/status.php
+++
@@ -1,7 +0,0 @@
- <?php 
- 
- //Takes raw data from the request
-$json = file_get_contents('php://input');
-
-// Converts it into a PHP object
-print $data = json_decode($json);
